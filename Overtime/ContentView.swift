a4acef7f8b5bcb66057135777abd2194e93d76ca
Jonas Frey <dev@jonasfrey.de>
2020-08-31 17:04:27 +0200

    Initial Commit

---
+++ Overtime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Overtime
+//
+//  Created by Jonas Frey on 31.08.20.
+//  Copyright © 2020 Jonas Frey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
