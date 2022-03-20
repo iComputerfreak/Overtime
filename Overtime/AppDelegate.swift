dfb51f6755db2c3d0ba6c3b8ddf41267b6b5884b
Jonas Frey <dev@jonasfrey.de>
2022-03-20 21:45:17 +0100

    Added migration to new datastructure

--- Overtime/AppDelegate.swift
+++ Overtime/AppDelegate.swift
@@ -8,21 +8,38 @@
 
 import UIKit
 
+struct OldOvertime: Decodable {
+    let date: Date
+    let duration: OldDuration
+}
+
+struct OldDuration: Decodable {
+    let seconds: Int
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let migrationV1Key = "migrationV1"
+    let migrationV2Key = "migrationV2"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         // Migrate the old data structure to the new structure
-        let migrated = UserDefaults.standard.bool(forKey: migrationV1Key)
-        if !migrated {
+        if !UserDefaults.standard.bool(forKey: migrationV1Key) {
             let overtimes: [Int: [Int: [Overtime]]] = loadV1()
             let migratedOvertimes: [Overtime] = overtimes.values.flatMap({ $0.values.joined() })
             saveV1(migratedOvertimes)
         }
+        
+        // MARK: Migration 2
+        if !UserDefaults.standard.bool(forKey: migrationV2Key) {
+            let overtimes: [OldOvertime] = loadV2()
+            let migrated = overtimes.map({ Overtime(date: $0.date, duration: Duration(seconds: $0.duration.seconds)) })
+            saveV2(migrated)
+        }
+        
         return true
     }
     
@@ -39,6 +56,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
+    private func loadV2() -> [OldOvertime] {
+        guard let plist = UserDefaults.standard.value(forKey: JFUtils.overtimesKey) as? Data else {
+            return []
+        }
+        do {
+            let values = try PropertyListDecoder().decode([OldOvertime].self, from: plist)
+            return values
+        } catch let e {
+            print(e)
+            return []
+        }
+    }
+    
     private func saveV1(_ overtimes: [Overtime]) {
         print("Migrating...")
         do {
@@ -49,6 +79,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print(e)
         }
     }
+    
+    private func saveV2(_ overtimes: [Overtime]) {
+        print("Migrating...")
+        do {
+            let plist = try PropertyListEncoder().encode(overtimes)
+            UserDefaults.standard.set(plist, forKey: JFUtils.overtimesKey)
+            UserDefaults.standard.set(true, forKey: migrationV2Key)
+        } catch let e {
+            print(e)
+        }
+    }
+
 
     // MARK: UISceneSession Lifecycle
 
