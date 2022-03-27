27f6b99805e9f6b0e4915f6422d5fc3c25629d89
Jonas Frey <dev@jonasfrey.de>
2022-03-27 15:59:51 +0200

    Fixed migration

--- Overtime/AppDelegate.swift
+++ Overtime/AppDelegate.swift
@@ -19,93 +19,72 @@ struct OldDuration: Decodable {
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     let migrationV1Key = "migrationV1"
     let migrationV2Key = "migrationV2"
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Migrate the old data structure to the new structure
-        if !UserDefaults.standard.bool(forKey: migrationV1Key) {
-            let overtimes: [Int: [Int: [Overtime]]] = loadV1()
-            let migratedOvertimes: [Overtime] = overtimes.values.flatMap({ $0.values.joined() })
-            saveV1(migratedOvertimes)
-        }
-        
-        // MARK: Migration 2
+        // MARK: Migration
         if !UserDefaults.standard.bool(forKey: migrationV2Key) {
-            let overtimes: [OldOvertime] = loadV2()
-            let migrated = overtimes.map({ Overtime(date: $0.date, duration: Duration(seconds: $0.duration.seconds)) })
-            saveV2(migrated)
+            do {
+                let overtimes: [Int: [Int: [OldOvertime]]] = try load()
+                var migrated: [Overtime] = []
+                for i in overtimes.values {
+                    for j in i.values {
+                        for overtime in j {
+                            migrated.append(Overtime(
+                                date: overtime.date,
+                                duration: Duration(seconds: overtime.duration.seconds))
+                            )
+                        }
+                    }
+                }
+                try save(migrated)
+                UserDefaults.standard.set(true, forKey: migrationV1Key)
+                UserDefaults.standard.set(true, forKey: migrationV2Key)
+            } catch let e {
+                print(e)
+                AlertHandler.showSimpleAlert(
+                    title: "Fehler beim Migrieren.",
+                    message: "Fehler beim Migrieren der alten Daten. Daten werden nicht überschrieben.")
+                fatalError()
+            }
         }
         
         return true
     }
     
-    private func loadV1() -> [Int: [Int: [Overtime]]] {
+    private func load() throws -> [Int: [Int: [OldOvertime]]] {
         guard let plist = UserDefaults.standard.value(forKey: JFUtils.overtimesKey) as? Data else {
             return [:]
         }
-        do {
-            let values = try PropertyListDecoder().decode([Int: [Int: [Overtime]]].self, from: plist)
-            return values
-        } catch let e {
-            print(e)
-            return [:]
-        }
+        let values = try PropertyListDecoder().decode([Int: [Int: [OldOvertime]]].self, from: plist)
+        return values
     }
     
-    private func loadV2() -> [OldOvertime] {
-        guard let plist = UserDefaults.standard.value(forKey: JFUtils.overtimesKey) as? Data else {
-            return []
-        }
-        do {
-            let values = try PropertyListDecoder().decode([OldOvertime].self, from: plist)
-            return values
-        } catch let e {
-            print(e)
-            return []
-        }
-    }
-    
-    private func saveV1(_ overtimes: [Overtime]) {
+    private func save(_ overtimes: [Overtime]) throws {
         print("Migrating...")
-        do {
-            let plist = try PropertyListEncoder().encode(overtimes)
-            UserDefaults.standard.set(plist, forKey: JFUtils.overtimesKey)
-            UserDefaults.standard.set(true, forKey: migrationV1Key)
-        } catch let e {
-            print(e)
-        }
+        let plist = try PropertyListEncoder().encode(overtimes)
+        UserDefaults.standard.set(plist, forKey: JFUtils.overtimesKey)
     }
     
-    private func saveV2(_ overtimes: [Overtime]) {
-        print("Migrating...")
-        do {
-            let plist = try PropertyListEncoder().encode(overtimes)
-            UserDefaults.standard.set(plist, forKey: JFUtils.overtimesKey)
-            UserDefaults.standard.set(true, forKey: migrationV2Key)
-        } catch let e {
-            print(e)
-        }
-    }
-
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
